48c3c3b88cf93a5c829bc8d51d9dd9d8cd6823a6
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-04-09 12:46:39 +0900

    Deviseの導線追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,7 +9,7 @@ module ApplicationHelper
   def flash_message(message, klass)
     content_tag(:div, class: "alert alert-#{klass}") do
       concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
-      safe_join(message)
+      concat h(message)
     end
   end
 end
