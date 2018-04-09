4cb43aca84908c374d82e1ccbba8c114ed0dbdf7
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-04-09 14:17:19 +0900

    ActionMailerの設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,13 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   #devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  #mailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    authentication: :plain,
+    user_name: Rails.application.secrets.SMTP_EMAIL,
+    password: Rails.application.secrets.SMTP_PASSWORD
+  }
 end
