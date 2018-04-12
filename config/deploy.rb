6f2ae54131f635764606979430e54a5d056005f8
kenji nakamura <kenji.nkmr.1117@gmail.com>
2018-04-12 17:17:16 +0900

    Capistarinoによるデプロイ設定

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock '~> 3.10.1'
 
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, 'https://github.com:KendyNakamura/AnaLive.git'
 
 # base
 set :application, 'Analive'
